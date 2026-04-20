5641a458351c42b7507ce1e8703b301045fb12fe
kucingoranye <ardiantow04@gmail.com>
2026-04-20 20:51:04 +0700

    manager: add signature for multi manager support like kowsu, rksu and wild ksu
    Signed-off-by: kucingoranye <ardiantow04@gmail.com>

--- drivers/kernelsu/manager/apk_sign.c
+++ drivers/kernelsu/manager/apk_sign.c
@@ -449,5 +449,9 @@ bool is_manager_apk(char *path)
 		return false;
 	}
 #endif
-	return check_v2_signature(path, EXPECTED_MANAGER_SIZE, EXPECTED_MANAGER_HASH);
+	return (check_v2_signature(path, EXPECTED_MANAGER_SIZE, EXPECTED_MANAGER_HASH)
+		|| check_v2_signature(path, 0x375, "484fcba6e6c43b1fb09700633bf2fb4758f13cb0b2f4457b80d075084b26c588")  // KOWX712/KernelSU
+		|| check_v2_signature(path, 0x396, "f415f4ed9435427e1fdf7f1fccd4dbc07b3d6b8751e4dbcec6f19671f427870b")  // rsuntk/KernelSU
+		|| check_v2_signature(path, 0x381, "52d52d8c8bfbe53dc2b6ff1c613184e2c03013e090fe8905d8e3d5dc2658c2e4")  // WildKernels/Wild_KSU
+	);
 }
